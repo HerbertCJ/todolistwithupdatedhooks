6c3354a4131a7579237b53142794491e7b2eab19
Herbert Cabus Jacby <111984596+HerbertCJ@users.noreply.github.com>
2023-01-05 12:47:20 -0300

    added submit a item to the todolist

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -6,15 +6,37 @@ export default function Container() {
     const [list, setList] = useState([])
     const [task, setTask] = useState('')
 
+    function handleChange(e) {
+        setTask(e.target.value)
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        let newTask = {
+            id: new Date().getTime(),
+            text: task,
+        }
+
+        setList([...list, newTask])
+        setTask('')
+    }
+
     return (
         <div className="main">
-            <form action="#">
+            <form action="#" onSubmit={handleSubmit}>
                 <h3>To Do List</h3>
-                <p>Write down here your note</p>     
-                <input type="text" />           
+                <p>Write down here your note</p>
+                <input type="text" onChange={handleChange} value={task} />
                 <button>Add</button>
             </form>
-            <div>               
+            <div>
+                {list.map(
+                    (item) => (
+                        <div key={item.text}>
+                            {item.text}
+                        </div>
+                    ))}
             </div>
         </div>
     )
